5c6ee070438fded436cd9aaced40b1d5d02bc62c
Gunnar Andersson <gandersson@genivi.org>
2020-05-18 16:54:52 +0200

    docker: Add curl test for HTTP GET to script
    Signed-off-by: Gunnar Andersson <gandersson@genivi.org>

--- docker/gen2-server/tests/test.sh
+++ docker/gen2-server/tests/test.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+r=0
+
 # There should be 6 screens if server starts OK
 ./W3CServer.sh startme
 NUM=$(screen -list | fgrep '(Detached)' | wc -l)
@@ -9,12 +11,26 @@ if [ $NUM -ne 6 ] ; then
    echo "Expected 6 running processes (screens)"
    echo "But instead there are $NUM.  Listing all screens:"
    screen -list
-   return 1
+   r=1
 else
    echo "Server start test: OK."
-   return 0
 fi
 
 
+# Now try interacting with the server
 
+NODE=Vehicle/Cabin/Door/Row1/Right/IsOpen
+PORT=8888
+
+curl -s http://127.0.0.1:$PORT/$NODE | fgrep -q '{"timestamp":'
+
+# Check success code from text match
+if [ $? -eq 0 ] ; then
+   echo "GET request test: OK."
+else
+   echo "GET request test: FAILED!"
+   r=1
+   exit 1
+fi
+exit $r
 
